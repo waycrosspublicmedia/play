bdff019b4746d38032bf65b63bef5b432389620f
tygszfa <185154684+genizy@users.noreply.github.com>
2025-03-13 16:04:40 -0700

    gym stack

---
+++ gym-stack/__settings__.js
@@ -0,0 +1,21 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "1961535.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "default"
+};
+window.SCRIPTS = [ 169872599, 169872602, 169872603, 169872604, 169872605, 169872607, 169872608, 169872613, 169872614, 169872615, 169872616, 169872617, 169872668, 169872622, 169872623, 169872624, 169872625, 169872626, 169872627, 169872628, 169872635, 169872639, 169872640, 169872642, 169872643, 169872663, 169872664, 169872707, 169872726, 169872727, 169872728, 169872730, 169872731, 169872738, 169872745, 169872768, 169872788, 169872792, 169872795, 169872796, 172739138, 172739144, 173245128 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+];
