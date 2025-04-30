aae289cdacc2e0d2cdc5af1acc2c320fbd70d8fc
bubbls <132022772+bubbls@users.noreply.github.com>
2025-04-29 18:50:25 -0600

    add freekick

---
+++ freekick-football/__settings__.js
@@ -0,0 +1,27 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "942276.json";
+window.CONTEXT_OPTIONS = {
+  'antialias': false,
+  'alpha': true,
+  'preserveDrawingBuffer': false,
+  'preferWebGl2': false,
+  'preferWebGpu': false,
+  'powerPreference': "high-performance"
+};
+window.SCRIPTS = [32163975, 32164171, 32164375, 32164277, 32163937, 32164341, 32164154, 32164163, 32164208, 32164150, 32164226, 32164340, 32164392, 32163941, 32164391, 32163943, 32163977, 32164343, 32164083, 32164356, 32164155, 32163936, 32163954, 32163887, 32164393, 33501892, 156874570];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+  useKeyboard: true,
+  useMouse: true,
+  useGamepads: false,
+  useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [{
+  'moduleName': 'Ammo',
+  'glueUrl': 'files/assets/32163963/1/ammo.wasm.js',
+  'wasmUrl': 'files/assets/32163876/1/ammo.wasm.wasm',
+  'fallbackUrl': 'files/assets/32164353/1/ammo.js',
+  'preload': true
+}, ];
